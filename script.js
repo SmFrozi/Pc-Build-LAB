ba4aa2912619f8423d8581692e040275ae3bb5ed
SmFrozi <xikdron9@gmail.com>
2026-03-05 11:33:59 +0200

    another update

--- script.js
+++ script.js
@@ -3,46 +3,38 @@
 let currentTotal = 0;
 const productsGrid = document.querySelector(".products-grid");
 const totalAmountElement = document.querySelector("#total-amount");
-const selectedItemContainer = document.querySelector("#selected-item");
-
-// ДОБАВИЛИ ТОЧКИ ПЕРЕД КЛАССАМИ
-const selectedName = selectedItemContainer.querySelector(".selected-name");
-const selectedPrice = selectedItemContainer.querySelector(".selected-price");
-const selectedImg = selectedItemContainer.querySelector(
-  ".selected-product-img",
-);
+const buildSummary = document.querySelector(".build-summary");
 
 const products = [
   {
     id: 1,
     name: "Amd Ryzen 5 5600",
-    price: 125,
+    price: 121,
     img: "img/E910_AMD_R5-5600_01.png",
   },
   {
     id: 2,
     name: "Intel Core i5-12400F",
-    price: 140,
-    img: "img/E910_AMD_R5-5600_01.png",
+    price: 118,
+    img: "img/e8b474_i5-12400f.png",
   },
   {
     id: 3,
     name: "Amd Ryzen 5 3600",
-    price: 45,
+    price: 53,
     img: "img/E910_AMD_R5-5600_01.png",
   },
   {
     id: 4,
     name: "Intel Core i5-7500",
-    price: 399,
-    img: "img/E910_AMD_R5-5600_01.png",
+    price: 389,
+    img: "img/e8b474_i5-12400f.png",
   },
 ];
 
 function renderProducts(items) {
   productsGrid.innerHTML = "";
   items.forEach((product) => {
-    // ДОБАВИЛИ data-img В КНОПКУ
     const html = `
       <article class="product-card">
         <img class="product-img" src="${product.img}" alt="${product.name}">
@@ -60,22 +52,30 @@ function renderProducts(items) {
 
 renderProducts(products);
 
-// ВСЯ ЛОГИКА ВНУТРИ ОДНОГО ОБРАБОТЧИКА
 productsGrid.addEventListener("click", function (e) {
   if (e.target.classList.contains("btn-add")) {
     const price = Number(e.target.dataset.price);
     const name = e.target.dataset.name;
     const img = e.target.dataset.img;
 
-    // Считаем общую сумму
-    currentTotal += price;
+    currentTotal = price;
     totalAmountElement.textContent = currentTotal;
 
-    // ОБНОВЛЯЕМ ПРАВУЮ КОЛОНКУ
-    selectedName.textContent = name;
-    selectedPrice.textContent = `${price} €`;
-    selectedImg.src = img;
+    const selectedHtml = `
+      <div id="selected-item">
+        <img class="selected-product-img" src="${img}" alt="${name}" />
+        <div class="selected-info">
+          <p class="selected-name">${name}</p>
+          <span class="selected-price">${price} €</span>
+        </div>
+      </div>
+    `;
 
-    console.log(`Добавлено: ${name}`);
+    const existingItem = document.querySelector("#selected-item");
+    if (existingItem) existingItem.remove();
+
+    buildSummary.insertAdjacentHTML("beforeend", selectedHtml);
+
+    console.log(`Цена заменена на: ${price}€`);
   }
 });
